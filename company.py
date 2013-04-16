3548964d938b5ee86ef2fec76e25a95688209cc7
Albert Cervera i Areny <albert@nan-tic.com>
2013-04-16 23:23:15 +0200

    pyflakes fixes.

--- company.py
+++ company.py
@@ -1,7 +1,7 @@
 #This file is part company_logo module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-from trytond.model import ModelView, ModelSQL, fields
+from trytond.model import fields
 from trytond.pool import PoolMeta
 
 __all__ = ['Company']
