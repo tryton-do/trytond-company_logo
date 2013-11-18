6b9739f311fa6f77ef2999039c5a90e6bcd1d351
resteve <resteve@zikzakmedia.com>
2013-11-18 10:07:44 +0100

    Change setup.py to use a better structure

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
+# encoding: utf-8
 #This file is part company_logo module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-
 from setuptools import setup
 import re
+import os
 import ConfigParser
+
+MODULE = 'company_logo'
+PREFIX = 'trytonspain'
+MODULE2PREFIX = {}
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -20,27 +29,31 @@ minor_version = int(minor_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+        prefix = MODULE2PREFIX.get(dep, 'trytond')
+        requires.append('%s_%s >= %s.%s, < %s.%s' %
+                (prefix, dep, major_version, minor_version,
+                major_version, minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytonspain_company_logo',
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
     description='Tryton module logo companies management',
-    author='NaN-tic',
-    author_email='info@NaN-tic.com',
-    url='http://www.nan-tic.com',
-    download_url="https://bitbucket.org/trytonspain/trytond-company_logo",
-    package_dir={'trytond.modules.company_logo': '.'},
+    long_description=read('README'),
+    author='NaN·tic',
+    url='http://www.nan-tic.com/',
+    download_url="https://bitbucket.org/trytonspain/trytond-%s" % MODULE,
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.company_logo',
-        'trytond.modules.company_logo.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.company_logo': info.get('xml', []) \
-            + ['tryton.cfg', 'locale/*.po'],
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -49,10 +62,15 @@ setup(name='trytonspain_company_logo',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'Intended Audience :: Manufacturing',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
+        'Natural Language :: Czech',
+        'Natural Language :: Dutch',
         'Natural Language :: English',
+        'Natural Language :: French',
+        'Natural Language :: German',
+        'Natural Language :: Russian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
@@ -64,8 +82,9 @@ setup(name='trytonspain_company_logo',
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    company_logo = trytond.modules.company_logo
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
+    tests_require=tests_require,
     )
