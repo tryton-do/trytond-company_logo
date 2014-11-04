c826849d532e274ee4293d34e0a2d707d9a0bf8d
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 15:49:07 +0100

    Migrate to version 3.4

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
-#This file is part company_logo module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part company_logo module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
-from .test_company import suite
+from .test_company_logo import suite
 
 __all__ = ['suite']
--- tests/test_company_logo.py
+++ tests/test_company_logo.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python
-#This file is part company_logo module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part company_logo module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class CompanyLogoTestCase(unittest.TestCase):
-    '''
-    Test CompanyLogo module.
-    '''
+    'Test CompanyLogo module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('company_logo')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('company_logo')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -40,6 +27,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             CompanyLogoTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
