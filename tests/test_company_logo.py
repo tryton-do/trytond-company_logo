4dbd3a9c8b801b1474ba34444985dd3a157591d1
resteve <resteve@zikzakmedia.com>
2015-12-01 11:05:52 +0100

    Tests
    --HG--
    branch : 3.8

--- tests/test_company_logo.py
+++ tests/test_company_logo.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-# This file is part company_logo module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the company_logo module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CompanyLogoTestCase(unittest.TestCase):
-    'Test CompanyLogo module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('company_logo')
-
-    def test0005views(self):
-        'Test views'
-        test_view('company_logo')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class CompanyLogoTestCase(ModuleTestCase):
+    'Test Company Logo module'
+    module = 'company_logo'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            CompanyLogoTestCase))
+        CompanyLogoTestCase))
     return suite
